89f266a0c0269ba180cf5bb5024fdc7c9549d962
Meisam Mofidi <meisam.mofidi@telus.com>
2021-08-18 15:02:36 -0400

    Added Congrats files

---
+++ src/Congrats.test.js
@@ -0,0 +1,9 @@
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+Enzyme.configure({adapter : new EnzymeAdapter()})
+
+
+test('should', () => {
+    
+})
