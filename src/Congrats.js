a170cd0f638a2a2f1cdd5f503297606c0eb6ab1f
Meisam Mofidi <meisam.mofidi@telus.com>
2021-08-18 16:03:54 -0400

    Added TDD Congrats test component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
+ */
+
+const Congrats = ({ success }) => {
+  if (!success) return <div data-test="congrats-component" />;
+  return (
+    <div data-test="congrats-component">
+      <span data-test="congrats-message">Congratulations! You guessed the word!</span>
+    </div>
+  );
+};
+
+export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,9 +1,29 @@
-import Enzyme, {shallow} from 'enzyme'
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Congrats from "./Congrats";
+import {findByTestAttr} from '../test/utils'
 
-Enzyme.configure({adapter : new EnzymeAdapter()})
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
 
-test('should', () => {
-    
-})
+test("should renders without error", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'congrats-component');
+    expect(component.length).toBe(1);
+});
+
+test("should display non-empty message when success is true", () => {
+    const wrapper = setup({success: true});
+    const component = findByTestAttr(wrapper, 'congrats-message')
+    expect(component.text().length).not.toBe(0);
+});
+
+test("should renders no text when 'success' prop is false", () => {
+    const wrapper = setup({success: false})
+    const message = findByTestAttr(wrapper, 'congrats-component')
+    expect(message.text()).toBe('');
+});
